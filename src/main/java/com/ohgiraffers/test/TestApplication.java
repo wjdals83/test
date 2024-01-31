82a8486201106ea7ec4d7dc14c82be8dc9b64972
jungmin <tjwjdals0803@naver.com>
2024-01-31 14:30:51 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+
+    // 여기는 주석입니다.
+    // 수업목표 오늘의 수업 목표입니다.
+
+
+    /*  여기는
+    * 여러 줄
+    * 주석 입니다.
+    * */
+
+    /*
+    * crtl - space : 자동완성 기능
+    * crtl - d : 복사*/
+    // 복사 예시
+    // 복사 예시
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다.");
+
+    }
+
+}
